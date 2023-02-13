257484a9a374ad7d0f94ffe032616b0806265da9
jossi <rolety2202@naver.com>
2023-02-14 04:12:28 +0900

    -조건 if , when ,Expression , Statement-

--- app/src/main/java/com/example/myapplication/sample1.kt
+++ app/src/main/java/com/example/myapplication/sample1.kt
@@ -37,3 +37,35 @@ fun hi(){
 }
 
 // 4. 조건식
+
+fun maxBy(a : Int , b : Int) : Int {
+    if(a>b){
+       return a
+    }else{
+        return b
+    }
+}
+
+fun maxBy2(a : Int, b: Int) = if(a>b) a else b
+//코틀린의 모든 함수는 Expression
+fun checkNum(score : Int) : Unit{
+    when (score) { //스위치 Statement
+        0 -> println("this is 0")
+        1 -> println("this is 1")
+        2,3 -> println("this is 2 or 3")// 복수 가능
+        else -> println(" I don1t know")
+    }
+    var b = when(score){ // 리턴값으로도 사용 가능 Expression
+        1 -> 1
+        2 -> 2
+        else -> 3
+    }
+    when(score){
+        in 90..100 -> println("You are genius")
+        in 10..80 -> println("not bad")
+        else -> println("okay")
+    }
+}
+// Expression vs Statement
+// Expression 뭘 만들어서 값을 반환하는 것
+// Statement 어떤 것을 명령을 하라고 지시하는 문장 같은 것
