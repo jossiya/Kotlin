aec3d9824e5c7324620bac3d15fd87257b2a0c2e
jossi <rolety2202@naver.com>
2023-02-16 01:44:41 +0900

    -class 설명-

--- app/src/main/java/com/example/myapplication/ClassPractice.kt
+++ app/src/main/java/com/example/myapplication/ClassPractice.kt
@@ -1,6 +1,6 @@
 package com.example.myapplication
 
-//class Human constructor(name : String){
+//class Human constructor(name : String){ // constructor 사용 방법
 //
 //
 //    val name =name
@@ -9,25 +9,25 @@ package com.example.myapplication
 //        println("This is so YUMMMY!")
 //    }
 //}
- open class Human(val name : String ="jogillra"){
-    constructor(name : String,age : Int): this(name){
+ open class Human(val name : String ="jogillra"){ // 파라미터로 선언 가능 하고 상속 할려면 open 붙여야함
+    constructor(name : String,age : Int): this(name){ //constructor 선언 방법
         println("My name is ${name}, ${age}years old")
     }
 
-    init {
+    init {//클레스 실행시 기본 실행되는 매서드
         println("New human has been born!!")
     }
     fun eatingCake(){
         println("This is so YUMMMY!")
     }
-    open fun singASong(){
+    open fun singASong(){//오버라이딩 하기위해 오픈
         println("lalala")
     }
 }
 
-class Korean : Human(){
+class Korean : Human(){// 상속은 자바랑 같은 하나만 가능
     override fun singASong(){
-        super.singASong()
+        super.singASong()// 자바랑 같음
         println("랄랄라")
         println("my name is : ${name}")
     }
@@ -39,6 +39,6 @@ fun main(){
 //
 //    val mom =Human("hohoya" , 56)
 //    println("this human`s is ${human.name}")
-    val korean= Korean();
+    val korean= Korean(); //자바랑 다르게 new 연산자 사용 할 필요가 없다.
     korean.singASong()
 }
